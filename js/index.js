eee2d77c5bcc5314bf5cbdfffb06f690128b6ec9
张中 <10781864@qq.com>
2016-10-30 10:04:03 +0800

    '00'

--- js/index.js
+++ js/index.js
@@ -10,7 +10,13 @@ $(function(){
         $('.list').empty();
         localStorage.data=JSON.stringify(todos)
      $(todos).each(function(i,v){
-        $("<li class='list-item'><div class='todo'>"+v.title+"</div><div class='close icon-font icon-delete'></div></li>").appendTo('.list')
+
+        $("<li class='list-item'><div class='todo'>"+v.title+"</div><div class='close icon-font icon-delete'></div></li>").addClass(function(){
+            if(v.state==1){
+                return "done";
+            }
+        }).appendTo('.list')
+
      })
    }
    //添加数据
@@ -19,83 +25,105 @@ $(function(){
        render();
        localStorage.data=JSON.stringify(todos)
    }
-
    //点击添加按钮，添加事件       // //新建便签中的删除按钮
     $('.add').on('click',function(){
         $('.add-box').css({display:'block'})
         $('.box').css({display:'none'})
         $('.delete').on('click',function(){
-            // $('.add-box').css({display:'none'})
-            // $('.box').css({display:'block'})
             $('.mask').css({display:'block'})
             $('.queding').on('click',function(){
                 $('.mask').css({display:'none'})
                 $('.add-box').css({display:'none'})
                 $('.box').css({display:'block'})
+                // $('.xiugai').css({display:'none'})
+                $('#add').val('');
             })
             $('.quxiao').on('click',function(){
                 $('.mask').css({display:'none'})
             })
         })
-
     })
+
     //点击返回按钮，获取到值插入到ul中
-    $('.fanhui').on('click',function(){
+    $('.fanhui').on('touchstart',function(){
         $('.add-box').css({display:'none'})
         $('.box').css({display:'block'})
         var val=$('textarea').val()
         if(val){
             push(val)
         }
+        $('#add').val('');
     })
     //点击皮肤
     $('.pifu').on('click',function(){
         $('.header-content').toggleClass('huaipifu')
         $('.footer').toggleClass('huaipifu')
+        $('.add-header').toggleClass('huaipifu')
+        $('.add-footer').toggleClass('huaipifu')
     })
 
 
     //遮罩
     var h=document.documentElement.clientHeight;
     $('.mask').height(h);
+
+
     // /修改标签内容
-    // $('.list').on('click','.list-item',function(){
-        // console.log($(this))
-        // $('.add-box').css({display:'block'})
-        // $('.box').css({display:'none'})
-
-
-    // })
-        //点击li里的删除按钮删除
-    $('.list .list-item').on('click','.close',function(){
-        var i=$(this).closest('.list-item').index();
-        todos.splice(i,1);
-        localStorage.data=JSON.stringify(todos)
-        $(this).closest('.list-item').remove();
+    var flag=true;
+    var index;
+    $('.list').on('click','.list-item',function(){
+           index=$(this).index()
+           var oldval=todos[$(this).index()].title;
+           $('#xiugai').val(oldval);
+           $('.add-box').css({display:'none'})
+           $('.box').css({display:'none'})
+           $('.xiugai-box').css({display:'block'})
     })
-    //
-    // $('.list').on('click','.list-item',function(){
-    //   $(this).addClass(function(){
-    //       return 'done';
-    //   });
-    // })
+    $('.xiugai').on('click',function(){
+        var newval=$('#xiugai').val();
+        todos[index].title=newval;
+        localStorage.data=JSON.stringify(todos);
+        render();
+        $('.add-box').css({display:'none'})
+        $('.box').css({display:'block'})
+        $('.xiugai-box').css({display:'none'})
+    })
+
+
+
+
+    //点击li里的删除按钮删除
+
+    $('.list').on('touchstart','.close',function(e){
+        // e.stopPropagation();
+        var i=$(this).index();
+        console.log(i)
+        todos.splice(i,1);
+        localStorage.data=JSON.stringify(todos);
+        render()
+        // $(this).closest('.list-item').remove();
+    })
+
+
     var left=null;
     $('.list').on('touchstart','.list-item',function(e){
         left=e.originalEvent.changedTouches[0].pageX
-        $(this).addClass(function(){
-                  return 'done';
-              });
     })
     $('.list').on('touchmove','.list-item',function(e){
         var n=e.originalEvent.changedTouches[0].pageX
         var m=left-n;
-       if(m>0&&m>40){
+       if(m>40){
            $(this).addClass('active').queue(function(){
-               $(this).delay(800).removeClass('active').dequeue()
+               $(this).removeClass('active').dequeue()
+
            })
+           $(this).addClass(function(){
+               return 'done';
+
+           });
+           todos[$(this).index()].state="1";
+           localStorage.data=JSON.stringify(todos)
+
        }
     })
-    // $('.list').on('touchend','.list-item',function(e){
-    //     $(this).addClass('ac')
-    // })
 })
